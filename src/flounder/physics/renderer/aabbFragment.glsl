7084e50c3af0c0c0f88c7a37b1f4282d622e56bf
Matthew Albrecht <mattparks5855@gmail.com>
2016-08-16 23:29:27 -0500

    Tweaks before redos.

--- src/flounder/physics/renderer/aabbFragment.glsl
+++ src/flounder/physics/renderer/aabbFragment.glsl
@@ -1,9 +1,6 @@
 #version 130
 
 layout(location = 0) out vec4 out_colour;
-layout(location = 1) out vec4 out_position;
-layout(location = 2) out vec4 out_normal;
-layout(location = 3) out vec4 out_specular;
 
 varying vec4 positionRelativeToCam;
 varying vec3 surfaceNormal;
@@ -12,7 +9,4 @@ uniform vec3 colour;
 
 void main(void) {
     out_colour = vec4(colour, 1.0);
-    out_position = positionRelativeToCam;
-    out_normal = vec4(surfaceNormal, 1.0);
-    out_specular = vec4(0.4);
 }
