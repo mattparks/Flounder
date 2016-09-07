cbb0feabafec28d68ad1a6bdc4b4ead3245c0253
Matthew Albrecht <mattparks5855@gmail.com>
2016-09-06 22:40:16 -0500

    Lots of orbit changes.

--- src/flounder/logger/FlounderLogger.java
+++ src/flounder/logger/FlounderLogger.java
@@ -13,6 +13,16 @@ public class FlounderLogger implements IModule {
 	public static final boolean LOG_TO_FILE = true;
 	public static final boolean ALLOW_LOUD_LOGS = FlounderLogger.class.getResource("/" + FlounderLogger.class.getName().replace('.', '/') + ".class").toString().startsWith("jar:");
 
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
 	private StringBuilder saveData;
 	private int linesRecorded;
 
