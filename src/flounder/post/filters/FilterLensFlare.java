e89048d0b461831e8f3039fe8640306f52459a8e
Matthew A <mattparks5855@gmail.com>
2017-02-03 19:37:32 -0600

    Basic lens flare.

--- src/flounder/post/filters/FilterLensFlare.java
+++ src/flounder/post/filters/FilterLensFlare.java
@@ -1,26 +1,43 @@
 package flounder.post.filters;
 
+import flounder.camera.*;
+import flounder.devices.*;
+import flounder.maths.matrices.*;
 import flounder.maths.vectors.*;
 import flounder.post.*;
 import flounder.resources.*;
 
 public class FilterLensFlare extends PostFilter {
-	private Vector3f sunPositon;
+	private Vector2f sunPositon;
+	private boolean flareHidden;
 
 	public FilterLensFlare() {
 		super("filterLensFlare", new MyFile(PostFilter.POST_LOC, "lensFlareFragment.glsl"));
-		sunPositon = new Vector3f();
+		sunPositon = new Vector2f();
+		flareHidden = false;
 	}
 
 	public void setSunPositon(Vector3f sunPositon) {
-		this.sunPositon.set(sunPositon);
+		Vector4f point4 = new Vector4f(sunPositon.x, sunPositon.y, sunPositon.z, 1);
+		point4 = Matrix4f.transform(FlounderCamera.getCamera().getViewMatrix(), point4, null);
+		point4 = Matrix4f.transform(FlounderCamera.getCamera().getProjectionMatrix(), point4, null);
+		Vector3f point = new Vector3f(point4);
+
+		point.x /= point.z;
+		point.y /= point.z;
+
+
+	//	point.x = (point.x + 1) * FlounderDisplay.getWidth() / 2;
+	//	point.y = (point.y + 1) * FlounderDisplay.getHeight() / 2;
+
+		this.sunPositon.set(point.x, point.y);
+		this.flareHidden = point.z <= 0.0f;
 	}
 
 	@Override
 	public void storeValues() {
-		// TODO: Move camera from 3D space to screen 2D.
-		float cameraX = 1.0f;
-		float cameraY = 0.0f;
-		shader.getUniformVec2("cameraPosition").loadVec2(cameraX, cameraY);
+		shader.getUniformVec2("sunPositon").loadVec2(sunPositon);
+		shader.getUniformFloat("aspectRatio").loadFloat(FlounderDisplay.getAspectRatio());
+		shader.getUniformBool("flareHidden").loadBoolean(flareHidden);
 	}
 }
