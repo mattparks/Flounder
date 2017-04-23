b8eb4cda534997c91b3a83518ea5e14ba857c3b1
Matthew Albrecht <mattparks5855@gmail.com>
2017-04-23 00:10:52 -0500

    Added static enemy, high score, and death.

---
+++ src/flounder/visual/BounceDriver.java
@@ -0,0 +1,38 @@
+package flounder.visual;
+
+import flounder.framework.*;
+
+/**
+ * A bounce driver that uses a sine wave.
+ */
+public class BounceDriver extends ValueDriver {
+	private float start;
+	private float amplitude;
+	private float length;
+	private float actime;
+
+	/**
+	 * Creates a new sine wave driver.
+	 *
+	 * @param start The start value.
+	 * @param end The end value.
+	 * @param length The length between two waves.
+	 */
+	public BounceDriver(float start, float end, float length) {
+		super(length);
+		this.start = start;
+		this.amplitude = end - start;
+		this.length = length;
+		this.actime = 0.0f;
+	}
+
+	@Override
+	protected float calculateValue(float time) {
+		float value = 0.5f + (float) Math.sin(time * Math.PI * 2.0) * 0.5f;
+		actime += Framework.getDelta();
+		if (actime > length / 2.0f) {
+			value = 0.0f;
+		}
+		return start + value * amplitude;
+	}
+}
--- src/flounder/visual/SinWaveDriver.java
+++ src/flounder/visual/SinWaveDriver.java
@@ -17,12 +17,12 @@ public class SinWaveDriver extends ValueDriver {
 	public SinWaveDriver(float min, float max, float length) {
 		super(length);
 		this.min = min;
-		amplitude = max - min;
+		this.amplitude = max - min;
 	}
 
 	@Override
 	protected float calculateValue(float time) {
-		float value = 0.5f + (float) Math.sin(time * Math.PI * 2) * 0.5f;
+		float value = 0.5f + (float) Math.sin(time * Math.PI * 2.0) * 0.5f;
 		return min + value * amplitude;
 	}
 }
