77ff2a6f85d908f1db88637b6a8b91179489ef59
Matthew Albrecht <mattparks5855@gmail.com>
2017-06-25 00:07:28 +0000

    Singleplayer world saving and loading kinda works.

--- src/flounder/entities/components/ComponentChild.java
+++ src/flounder/entities/components/ComponentChild.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 
 public class ComponentChild extends IComponentEntity implements IComponentEditor {
 	private Entity parent;
+	private ChildRemoved childRemoved;
 
 	/**
 	 * Creates a new ComponentChild.
@@ -27,6 +28,21 @@ public class ComponentChild extends IComponentEntity implements IComponentEditor
 		super(entity);
 
 		this.parent = parent;
+		this.childRemoved = null;
+	}
+
+	/**
+	 * Creates a new ComponentChild.
+	 *
+	 * @param entity The entity this component is attached to.
+	 * @param parent The parent to this child.
+	 * @param childRemoved A function called when the child has been removed.
+	 */
+	public ComponentChild(Entity entity, Entity parent, ChildRemoved childRemoved) {
+		super(entity);
+
+		this.parent = parent;
+		this.childRemoved = childRemoved;
 	}
 
 	@Override
@@ -59,5 +75,13 @@ public class ComponentChild extends IComponentEntity implements IComponentEditor
 
 	@Override
 	public void dispose() {
+		if (childRemoved != null) {
+			childRemoved.remove();
+		}
+	}
+
+	@FunctionalInterface
+	public interface ChildRemoved {
+		void remove();
 	}
 }
